304d57934da5350da43fab460d6f922d524494fd
zalman0813 <zalman0813@gmail.com>
2020-10-08 11:34:48 +0800

    added project folder test case

--- project_tempate.py
+++ project_tempate.py
@@ -1,4 +1,8 @@
+import os
+
+
 class ProjectTemplate(object):
-    def generate(self, project_name):
+    def generate(self, project_name, dir_path):
         if not project_name:
             raise ValueError('No project name')
+        os.mkdir(os.path.join(dir_path, project_name))
--- test_project_template.py
+++ test_project_template.py
@@ -1,3 +1,6 @@
+import filecmp
+import os
+import shutil
 import unittest
 
 from project_tempate import ProjectTemplate
@@ -6,12 +9,24 @@ from project_tempate import ProjectTemplate
 class ProjectTemplateTest(unittest.TestCase):
 
     def setUp(self):
+        self.project_name = 'watson'
         self.template = ProjectTemplate()
+
+    def tearDown(self):
+        if os.path.exists(self.project_name):
+            shutil.rmtree(self.project_name)
 
     def test_no_project_name(self):
         with self.assertRaises(ValueError):
-            self.template.generate('')
+            self.template.generate('', '.')
 
+    def test_project_folder_exists_with_project_name(self):
+        self.template.generate(f'{self.project_name}', '.')
+
+        cmp = filecmp.dircmp(self.project_name, f'test_example/template_case1/{self.project_name}')
+        self.assertEqual(cmp.diff_files, [])
+        self.assertEqual(cmp.left_only, [])
+        self.assertEqual(cmp.right_only, [])
 
 
 if __name__ == '__main__':
