a3406338f77f9672a4509fa344236dec1c7a2efe
zalman0813 <zalman0813@gmail.com>
2020-10-08 14:07:52 +0800

    added a fail test for other folder

--- project_tempate.py
+++ project_tempate.py
@@ -1,8 +1,21 @@
+import glob
 import os
+import re
 
 
 class ProjectTemplate(object):
-    def generate(self, project_name, dir_path):
+    def generate(self, project_name, dir_path, template_dir=None):
         if not project_name:
-            raise ValueError('No project name')
-        os.mkdir(os.path.join(dir_path, project_name))
+            raise ValueError('No tests name')
+        root_directory = f'{project_name}-project-development'
+        os.mkdir(os.path.join(dir_path, root_directory))
+
+        if template_dir:
+            for file_path in glob.glob(os.path.join(template_dir, '*')):
+                new_dir = os.path.join(dir_path, root_directory)
+                search_end_idx = re.search('template/', file_path).end()
+                new_path = os.path.join(new_dir, file_path[search_end_idx:]).replace('/project',
+                                                                                     f'/{project_name}_project')
+                object_name = file_path.split('/')[-1]
+                if object_name == 'project' and os.path.isdir(file_path):
+                    os.mkdir(new_path)
--- test_project_template.py
+++ test_project_template.py
@@ -10,20 +10,31 @@ class ProjectTemplateTest(unittest.TestCase):
 
     def setUp(self):
         self.project_name = 'watson'
+        self.root_directory = f'{self.project_name}-project-development'
         self.template = ProjectTemplate()
 
     def tearDown(self):
-        if os.path.exists(self.project_name):
-            shutil.rmtree(self.project_name)
+        if os.path.exists(self.root_directory):
+            shutil.rmtree(self.root_directory)
 
     def test_no_project_name(self):
         with self.assertRaises(ValueError):
             self.template.generate('', '.')
 
-    def test_project_folder_exists_with_project_name(self):
-        self.template.generate(f'{self.project_name}', '.')
+    def test_if_root_folder_exists_with_project_name(self):
+        self.template.generate(f'{self.project_name}', '.', None)
+        self.template_should_be_equal('template_case1')
 
-        cmp = filecmp.dircmp(self.project_name, f'test_example/template_case1/{self.project_name}')
+    def test_if_project_folder_exists_with_project_name(self):
+        self.template.generate(f'{self.project_name}', '.', 'test_example/template_case2/template')
+        self.template_should_be_equal('template_case2')
+
+    def test_if_other_folder_exists_with_project_name(self):
+        self.template.generate(f'{self.project_name}', '.', 'test_example/template_case3/template')
+        self.template_should_be_equal('template_case3')
+
+    def template_should_be_equal(self, test_case):
+        cmp = filecmp.dircmp(self.root_directory, f'test_example/{test_case}/{self.root_directory}')
         self.assertEqual(cmp.diff_files, [])
         self.assertEqual(cmp.left_only, [])
         self.assertEqual(cmp.right_only, [])
